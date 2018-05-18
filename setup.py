98cf3e7652694cb52dc207374f61d66a08435195
Daniel Wang <danielwpz@gmail.com>
2018-05-18 16:02:01 -0700

    Publish under own domain

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='yahoo_earnings_calendar',
+    name='dw_yahoo_earnings_calendar',
     packages=['yahoo_earnings_calendar'],
     install_requires=[
         'requests'
     ],
-    version='0.3.1',
+    version='0.0.1',
     description='Scrapes data from Yahoo! Finance earnings calendar',
-    author='Wenbo Yu',
-    author_email='wenboyu2@gmail.com',
-    url='https://github.com/wenboyu2/yahoo-earnings-calendar',
-    download_url='https://github.com/wenboyu2/yahoo-earnings-calendar/tarball/0.3.1',
+    author='Daniel Wang',
+    author_email='danielwpz@gmail.com',
     keywords=['stock', 'earnings', 'yahoo', 'scrape', 'finance'],
     classifiers=[],
 )
